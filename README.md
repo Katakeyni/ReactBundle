a5cd2e3a1532cffcf43a1953434c90b337fd8f1b
Katakeyni <ahmed.abdbassir@gmail.com>
2015-09-16 13:20:19 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ReactSymfony
+============
+
+A Symfony project created on September 16, 2015, 2:49 pm.
---
+++ src/Katakeyni/ReactBundle/KatakeyniReactBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Katakeyni\ReactBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KatakeyniReactBundle extends Bundle
+{
+}
